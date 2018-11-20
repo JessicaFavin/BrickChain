628fd5e05c21fdb19a54451715f368a87f2e17cd
JessicaFavin <jessica00228@hotmail.fr>
2018-11-20 11:02:08 +0100

    Fix test

--- BrickChain.java
+++ BrickChain.java
@@ -1,18 +1,16 @@
-import java.security.MessageDigest;
-
 import org.bouncycastle.jcajce.provider.digest.SHA3.DigestSHA3;
 import org.bouncycastle.jcajce.provider.digest.SHA3.Digest256;
 import org.bouncycastle.util.encoders.Hex;
+import java.security.MessageDigest;
 
 public class BrickChain {
     public static void main(String[] args) {
         System.out.println(sha3(""));
     }
 
-    public void testSha3(String input) {
-        // = "Hello world !";
+    public static String sha3(String input) {
         DigestSHA3 digestSHA3 = new Digest256();
         byte[] digest = digestSHA3.digest(input.getBytes());
-        System.out.println("SHA3-256 = " + Hex.toHexString(digest));
+         return Hex.toHexString(digest);
     }
 }
